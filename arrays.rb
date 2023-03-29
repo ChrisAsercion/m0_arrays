cfc71f56f25759510b50f313841ac2c970c82c1e
Chris Asercion <chris.asercion@gmail.com>
2023-03-29 10:31:36 -0600

    Add variables

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+friends = ["Jennifer", "Lisa", "David", "Courteney", "Matthew", "Matt"]
+scrabble_wins = [4, 0, 2, 8, 1, 5]
+bank_charges = [25.04, 88.38, 1.1, 40.70, 30.2, 20.3]
+debts_paid = [false, true, false, true]
